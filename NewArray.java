0ad30cfd125dd758bbeed03e2bc8a6996fd06f8b
sergii.ozackii <46957601+sergiiozackii@users.noreply.github.com>
2019-02-06 14:24:22 +0200

    Add files via upload

---
+++ NewArray.java
@@ -0,0 +1,16 @@
+package _05;
+
+//Найти индекс элемента, значение которого равно 15
+public class NewArray {
+    public static void main(String[] arg) {
+            int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+           for (int i = 0; i < array1.length; i++) {
+               if (array1[i] == 15) {
+                   System.out.println(i);
+
+    }
+
+
+}
+    }
+}
